32a683368e562a2ddd28c630064e14cc7d15bf75
GitHub Workflow Agent <github-workflow-agent@github.com>
2020-11-30 04:10:48 +0000

    Format for upm

--- Runtime/NetworkClient.cs
+++ Runtime/NetworkClient.cs
@@ -129,7 +129,6 @@ namespace Mirror
 
             // create server connection to local client
             NetworkServer.SetLocalConnection(connectionToClient);
-
         }
         /// <summary>
         /// connect host mode
@@ -271,11 +270,12 @@ namespace Mirror
 
         internal static void Update()
         {
-            // local or remote connection?
+            // local connection?
             if (connection is ULocalConnectionToServer localConnection)
             {
                 localConnection.Update();
             }
+            // remote connection?
             else
             {
                 // only update things while connected
